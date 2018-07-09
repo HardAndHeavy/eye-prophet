1d9b45fabdbcfb841e0f91b1aba56e604ae012c7
HardAndHeavy <HardAndHeavyMetal@gmail.com>
2018-07-09 23:24:13 +0300

    change routing

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM python:3.6-alpine
 
-WORKDIR /app
-ADD ./app /app
-
 RUN apk --no-cache --update-cache add gcc g++ gfortran build-base wget freetype-dev libpng-dev openblas-dev python-dev python3-dev
 RUN ln -s /usr/include/locale.h /usr/include/xlocale.h
 
@@ -13,5 +10,8 @@ RUN pip3 install --upgrade --no-cache-dir numpy
 RUN pip3 install --upgrade --no-cache-dir fbprophet
 
 EXPOSE 80
+
+WORKDIR /app
+ADD ./app /app
 
 CMD ["python", "app.py"]
--- app/app.py
+++ app/app.py
@@ -4,16 +4,18 @@ from _prophet import forecast
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST'])
+@app.route("/")
 def root():
+    html = "<h3>Welcome to eye-prophet!</h3>" \
+           "<p>This is web server for forecasting time series data based on the <a href='https://facebook.github.io/prophet'>Prophet</a> v{version}"
+    import fbprophet
+    return html.format(version=fbprophet.__version__)
+
+@app.route("/forecast", methods=['POST'])
+def makeForecast():
     params = request.get_json(silent=True)
     result = forecast(params)
     return result.to_json(orient='split')
-
-@app.route("/version")
-def version():
-    import fbprophet
-    return fbprophet.__version__
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
