21cf8e32c72dc81fb866cbd8882873390ae64651
HardAndHeavy <HardAndHeavyMetal@gmail.com>
2018-07-10 07:58:23 +0300

    add version

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import os
+from fbprophet import __version__
 from _prophet import forecast
 
 app = Flask(__name__)
@@ -8,14 +9,17 @@ app = Flask(__name__)
 def root():
     html = "<h3>Welcome to eye-prophet!</h3>" \
            "<p>This is web server for forecasting time series data based on the <a href='https://facebook.github.io/prophet'>Prophet</a> v{version}"
-    import fbprophet
-    return html.format(version=fbprophet.__version__)
+    return html.format(version=__version__)
 
 @app.route("/forecast", methods=['POST'])
 def makeForecast():
     params = request.get_json(silent=True)
     result = forecast(params)
     return result.to_json(orient='split')
+
+@app.route("/version")
+def version():
+    return __version__
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
